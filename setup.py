77da5230a7891895f6da1668f7f32cc48458b8bc
sm1ky <145751659+sm1ky@users.noreply.github.com>
2024-06-29 02:24:17 +0300

    Bump version: 0.1.2 → 0.1.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="marzban",
-    version="0.1.2",
+    version="0.1.3",
     packages=find_packages(),
     install_requires=[
         "httpx>=0.23.0",
