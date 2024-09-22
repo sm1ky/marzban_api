6bd25cab7a283eb5fe4c56ab1ab0387dc17b78de
Artem <deploy@sm1ky.com>
2024-09-22 04:38:50 +0000

    Update version: 0.2.1 → 0.2.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="marzban",
-    version="0.2.1",
+    version="0.2.2",
     packages=find_packages(),
     install_requires=[
         "httpx>=0.23.0",
