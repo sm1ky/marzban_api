f9c929d73d6b4f6e173b76c322c6081b675ad8db
sm1ky <145751659+sm1ky@users.noreply.github.com>
2024-09-22 06:36:26 +0200

    Update python package, add default import

--- marzban/__init__.py
+++ marzban/__init__.py
@@ -1,4 +1,62 @@
 from .api import MarzbanAPI
-from .models import *
+from .models import (
+    Admin, 
+    AdminCreate, 
+    AdminModify, 
+    UserCreate, 
+    UserModify, 
+    UserResponse, 
+    UsersResponse, 
+    UserStatus, 
+    UserTemplateCreate, 
+    UserTemplateModify, 
+    UserTemplateResponse, 
+    UserUsageResponse, 
+    UserUsagesResponse, 
+    NodeUsageResponse, 
+    NodesUsageResponse, 
+    NodeCreate, 
+    NodeModify, 
+    NodeResponse, 
+    SystemStats, 
+    CoreStats, 
+    ProxySettings, 
+    ProxyHost, 
+    ProxyInbound, 
+    Token, 
+    HTTPValidationError, 
+    ValidationError
+)
 
 __version__ = "0.2.0"
+
+__all__ = (
+    "__version__",
+    "MarzbanAPI",
+    "Admin", 
+    "AdminCreate", 
+    "AdminModify", 
+    "UserCreate", 
+    "UserModify", 
+    "UserResponse", 
+    "UsersResponse", 
+    "UserStatus", 
+    "UserTemplateCreate", 
+    "UserTemplateModify", 
+    "UserTemplateResponse", 
+    "UserUsageResponse", 
+    "UserUsagesResponse", 
+    "NodeUsageResponse", 
+    "NodesUsageResponse", 
+    "NodeCreate", 
+    "NodeModify", 
+    "NodeResponse", 
+    "SystemStats", 
+    "CoreStats", 
+    "ProxySettings", 
+    "ProxyHost", 
+    "ProxyInbound", 
+    "Token", 
+    "HTTPValidationError", 
+    "ValidationError"
+)
