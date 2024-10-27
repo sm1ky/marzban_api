5a8791a8ecd3b618aa5ca5252c43107b3825dc5d
sm1ky <145751659+sm1ky@users.noreply.github.com>
2024-10-27 12:46:17 +0100

    Add documentation url to pypi

--- setup.py
+++ setup.py
@@ -19,6 +19,12 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/sm1ky/marzban_api",
+    project_urls={
+        "Homepage": "https://github.com/sm1ky/marzban_api",
+        "Documentation [EN]": "https://github.com/sm1ky/marzban_api/blob/development/.readme/README_en.md", 
+        "Documentation [RU]": "https://github.com/sm1ky/marzban_api/blob/development/.readme/README_ru.md", 
+        "Source": "https://github.com/sm1ky/marzban_api",
+    },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
