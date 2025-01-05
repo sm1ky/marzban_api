36e7b794b1da4cf429bf86a6dd465ac5e78b74c2
sm1ky <145751659+sm1ky@users.noreply.github.com>
2025-01-05 15:32:04 +0100

    Release: 0.4.0 with full support Marzban v0.8.0 [skip bump]

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="marzban",
-    version="0.3.3",
+    version="0.4.0",
     packages=find_packages(),
     install_requires=[
         "httpx>=0.23.0",
